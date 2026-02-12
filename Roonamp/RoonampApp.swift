241a85da79f986562e6a131d703939ee02be3cb4
stuart-flowlens <stuart@flowlens.com>
2026-02-12 09:10:29 +0000

    Initial Commit

---
+++ Roonamp/RoonampApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoonampApp.swift
+//  Roonamp
+//
+//  Created by Stuart Gibson on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct RoonampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
